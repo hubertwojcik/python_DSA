b149e400d2f04d925e4ffc7e85463d076b4c3745
Hubert Wójcik <hubertwojcik@macbookpro-1.home>
2025-01-25 08:56:38 +0100

    dll pop

--- Doubly Linked Lists/dll.py
+++ Doubly Linked Lists/dll.py
@@ -28,9 +28,24 @@ class DoubleLinkedList:
             self.tail = new_node
         self.length += 1
         return True
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1   
+        return temp
 
 
 
 my_doubly_linked_list = DoubleLinkedList(1)
 my_doubly_linked_list.append(2)
+my_doubly_linked_list.pop()
 my_doubly_linked_list.print_list()
