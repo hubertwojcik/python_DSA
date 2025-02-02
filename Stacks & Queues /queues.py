a20982aae342ea42a138de561d33fa66c3cae196
Hubert W <hwojcikk@gmail.com>
2025-02-02 12:14:34 +0100

    queue: dequeue

--- Stacks & Queues /queues.py
+++ Stacks & Queues /queues.py
@@ -26,6 +26,19 @@ class Queue:
             self.last = new_node
         self.length += 1
 
+    def dequeue(self):
+        if self.first is None:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last = None
+        else:
+            self.first = self.first
+            temp.next = None
+        self.length -= 1
+        return temp
+
 
 my_queue = Queue(5)
 my_queue.print_queue()
