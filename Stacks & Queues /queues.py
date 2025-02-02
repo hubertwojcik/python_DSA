f74a176e0445856d1dbed5eab5442200b6546631
Hubert W <hwojcikk@gmail.com>
2025-02-02 11:35:13 +0100

    queue: create

---
+++ Stacks & Queues /queues.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Queue:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.first = new_node
+        self.last = new_node
+        self.length = 1
+
+my_queue = Queue(5)
