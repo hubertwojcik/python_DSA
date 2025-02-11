511c32ab1be5c078e8d2f09e70ac5bf84358e526
Hubert W <hwojcikk@gmail.com>
2025-02-11 19:35:07 +0100

    heap: change name

--- Heaps/heap.py
+++ Heaps/heap.py
@@ -16,11 +16,11 @@ class MaxHeap:
     
     def insert(self, value):
         self.heap.append(value)
-        current  = len(self.heap) - 1
+        current_index  = len(self.heap) - 1
 
-        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
-            self._swap(current, self._parent(current))
-            current = self._parent(current)
+        while current_index > 0 and self.heap[current_index] > self.heap[self._parent(current_index)]:
+            self._swap(current_index, self._parent(current_index))
+            current_index = self._parent(current_index)
         
     def remove(self):
         if len(self.heap) == 0:
@@ -34,4 +34,25 @@ class MaxHeap:
         self._sink_down(0)
 
         return max_value
+
+    def _sink_down(self, index):
+        max_index = index
+        
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if (left_index < len(self.heap)) and
+                    self.heap[left_index] > self.heap[max_index]:
+                max_index = left_index
+
+            if (right_index < len(self.heap)) and 
+                    self.heap[left_index] > self.heap[max_index]:
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
         
